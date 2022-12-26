aade265ed9e5f2590e0bf770aeac4e8bf98b9bac
AntonioSPR <antonio.prieto.riera@gmail.com>
2022-12-26 13:30:13 +0100

    Day 10, First puzzle done. The solution is out of competition, Christmas has already passed.

--- src/main/kotlin/day09.kt
+++ src/main/kotlin/day09.kt
@@ -66,7 +66,6 @@ fun day09Part2(input: String) {
     inputLines.forEach{
         val direction = it.split(' ')[0]
         val distance = it.split(' ')[1].toInt()
-        println("\n$direction $distance" )
         for (i in 1..distance) {
             when(direction) {
                 "U" -> longRope.moveUp()
@@ -74,10 +73,8 @@ fun day09Part2(input: String) {
                 "D" -> longRope.moveDown()
                 "R" -> longRope.moveRight()
             }
-            println("\t$longRope")
             tailPositions.add(longRope.tail.toString())
         }
-        println("\n ${tailPositions.size} $tailPositions")
     }
 
     var text = "--- Part Two ---\n"
@@ -155,36 +152,39 @@ class LongRope(val body: Array<Point> = Array(10){ Point(0, 0) } ) {
         }
         private set
 
-    fun moveUp(){
+    fun moveUp() {
         head.y++
         moveThatBody()
     }
-    fun moveRight(){
+
+    fun moveRight() {
         head.x++
         moveThatBody()
     }
-    fun moveDown(){
+
+    fun moveDown() {
         head.y--
         moveThatBody()
     }
-    fun moveLeft(){
+
+    fun moveLeft() {
         head.x--
         moveThatBody()
     }
 
-    private fun moveThatBody(){
-        for(i in 0 until (body.size - 1) ) {
+    private fun moveThatBody() {
+        for (i in 0 until (body.size - 1)) {
             val firstKnot = body[i]
-            val secondKnot = body[i+1]
-            when(firstKnot.x - secondKnot.x){
+            val secondKnot = body[i + 1]
+            when (firstKnot.x - secondKnot.x) {
                 2 -> {
                     secondKnot.x++
                     when (firstKnot.y - secondKnot.y) {
                         -2 -> secondKnot.y--
                         -1 -> secondKnot.y--
-                        0 ->  print("")
-                        1 ->  secondKnot.y++
-                        2 ->  secondKnot.y++
+                        0 -> print("")
+                        1 -> secondKnot.y++
+                        2 -> secondKnot.y++
                         else -> println("Error: fun moveThatBody() ΔX=2 ")
                     }
                 }
@@ -195,8 +195,8 @@ class LongRope(val body: Array<Point> = Array(10){ Point(0, 0) } ) {
                             secondKnot.y--
                         }
                         -1 -> print("")
-                        0 ->  print("")
-                        1 ->  print("")
+                        0 -> print("")
+                        1 -> print("")
                         2 -> {
                             secondKnot.x++
                             secondKnot.y++
@@ -204,23 +204,23 @@ class LongRope(val body: Array<Point> = Array(10){ Point(0, 0) } ) {
                         else -> println("Error: fun moveThatBody() ΔX=1")
                     }
                 }
-                0 ->  when (firstKnot.y - secondKnot.y) {
+                0 -> when (firstKnot.y - secondKnot.y) {
                     -2 -> secondKnot.y--
                     -1 -> print("")
-                    0  -> print("")
-                    1  -> print("")
-                    2  -> secondKnot.y++
+                    0 -> print("")
+                    1 -> print("")
+                    2 -> secondKnot.y++
                     else -> println("Error: fun moveThatBody() ΔX=0")
                 }
                 -1 -> {
-                    when (firstKnot.y - secondKnot.y)  {
+                    when (firstKnot.y - secondKnot.y) {
                         -2 -> {
                             secondKnot.x--
                             secondKnot.y--
                         }
                         -1 -> print("")
-                        0 ->  print("")
-                        1 ->  print("")
+                        0 -> print("")
+                        1 -> print("")
                         2 -> {
                             secondKnot.x--
                             secondKnot.y++
@@ -233,9 +233,9 @@ class LongRope(val body: Array<Point> = Array(10){ Point(0, 0) } ) {
                     when (firstKnot.y - secondKnot.y) {
                         -2 -> secondKnot.y--
                         -1 -> secondKnot.y--
-                        0 ->  print("")
-                        1 ->  secondKnot.y++
-                        2 ->  secondKnot.y++
+                        0 -> print("")
+                        1 -> secondKnot.y++
+                        2 -> secondKnot.y++
                         else -> println("Error: fun moveThatBody() ΔX=-2")
                     }
                 }
@@ -249,122 +249,4 @@ class LongRope(val body: Array<Point> = Array(10){ Point(0, 0) } ) {
     override fun toString(): String {
         return "[${body.joinToString(", ")}]"
     } // ---------- override fun toString(): String {
-
-} // ---------- class Rope(var head : Point = Point(0,0), var tail : Point  = Point(0,0)){
-
-//    private fun moveThatBody(){ // V1
-//        for(i in 0 until (body.size - 1) ) {
-//            val firstKnot = body[i]
-//            val secondKnot = body[i+1]
-//            // Diagonal movements
-//            if ((firstKnot.x - secondKnot.x).absoluteValue == 2 && (firstKnot.y - secondKnot.y).absoluteValue == 2 ){
-//                when (firstKnot.x - secondKnot.x) {
-//                    2 -> {
-//                        secondKnot.x++
-//                        secondKnot.y++
-//                    }
-//                    -2 -> {
-//                        secondKnot.x--
-//                        secondKnot.y--
-//                    }
-//                }
-//            }
-//            else {// NO diagonal movements
-//                when (firstKnot.x - secondKnot.x) {
-//                    2 -> {
-//                        secondKnot.x++
-//                        secondKnot.y = firstKnot.y
-//                    }
-//                    -2 -> {
-//                        secondKnot.x--
-//                        secondKnot.y = firstKnot.y
-//                    }
-//                }
-//                when (firstKnot.y - secondKnot.y) {
-//                    2 -> {
-//                        secondKnot.y++
-//                        secondKnot.x = firstKnot.x
-//                    }
-//                    -2 -> {
-//                        secondKnot.y--
-//                        secondKnot.x = firstKnot.x
-//                    }
-//                }
-//            }
-//        }
-//    } // ---------- private fun moveThatBody(){
-
-
-//private fun moveThatBody(){  // V2
-//    for(i in 0 until (body.size - 1) ) {
-//        val firstKnot = body[i]
-//        val secondKnot = body[i+1]
-//
-//        if((firstKnot.x - secondKnot.x) == 0 || (firstKnot.y - secondKnot.y).absoluteValue == 0 ){
-//            // firstKnot and secondKnot in the same line
-//            if ((firstKnot.x - secondKnot.x) == 2)
-//                secondKnot.x++
-//            else
-//                if ((firstKnot.x - secondKnot.x) == -2)
-//                    secondKnot.x--
-//                else
-//                    if ((firstKnot.y - secondKnot.y) == 2)
-//                        secondKnot.y++
-//                    else
-//                        if ((firstKnot.y - secondKnot.y) == -2)
-//                            secondKnot.y--
-//                        else
-//                            println("Error linea 193")
-//        }
-//        else
-//            if ((firstKnot.x - secondKnot.x) == 2 && (firstKnot.y - secondKnot.y).absoluteValue == 2 ){
-//                // firstKnot and secondKnot in diagonal, 2 squares apart
-//                if ((firstKnot.x - secondKnot.x) == 2)
-//                    if ((firstKnot.y - secondKnot.y) == 2){
-//                        secondKnot.x++
-//                        secondKnot.y++
-//                    }
-//                    else {
-//                        secondKnot.x++
-//                        secondKnot.y--
-//                    }
-//                else // firstKnot.x - secondKnot.x) == -2
-//                    if ((firstKnot.y - secondKnot.y) == 2){
-//                        secondKnot.x--
-//                        secondKnot.y++
-//                    }
-//                    else {
-//                        secondKnot.x--
-//                        secondKnot.y--
-//                    }
-//            }
-//            else
-//                if ((firstKnot.x - secondKnot.x) == 1 || (firstKnot.y - secondKnot.y).absoluteValue == 1 ){
-//                    // firstKnot and secondKnot are in a knight hop
-//                    if((firstKnot.x - secondKnot.x) == 2){
-//                        secondKnot.x++
-//                        secondKnot.y = firstKnot.y
-//                    }
-//                    else
-//                        if ((firstKnot.x - secondKnot.x) == -2 ){
-//                            secondKnot.x--
-//                            secondKnot.y = firstKnot.y
-//                        }
-//                        else
-//                            if((firstKnot.y - secondKnot.y) == 2){
-//                                secondKnot.y++
-//                                secondKnot.x = firstKnot.x
-//                            }
-//                            else
-//                                if ((firstKnot.y - secondKnot.y) == -2 ){
-//                                    secondKnot.y--
-//                                    secondKnot.x = firstKnot.x
-//                                }
-//                                else
-//                                    println("Error linea 240")
-//                }
-//                else
-//                    println("Error linea 243")
-//
-//    } // ---------- for(i in 0 until (body.size - 1) ) {
-//} // ---------- private fun moveThatBody(){
+} // ---------------------------------------- class LongRope(val body: Array<Point> = Array(10){ Point(0, 0) } ) {
--- src/main/kotlin/day10.kt
+++ src/main/kotlin/day10.kt
@@ -1,4 +1,10 @@
 // --- Day 10: Cathode-Ray Tube ---
+// addx V takes two cycles to complete. After two cycles, the X register is increased by the value V. (V can be negative.)
+// noop takes one cycle to complete. It has no other effect.
+// Signal strength is the cycle number multiplied by the value of the X register
+// Part 1: Find the sum of signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles.
+// your answer is too low 13300 13100 14340
+// Part 2: What eight capital letters appear on your CRT?
 
 fun day10(){
     val title = """
@@ -24,18 +30,92 @@ fun day10(){
 
 } // --------------------------------------------- fun day10(){
 
+
 fun day10Part1(input: String) {
-    //
+    // Part 1: Find the sum of signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles.
+    val register = Register()
+    val inputLines = input.lines()
+
+    inputLines.forEach{ line ->
+        if (line[0] == 'a') { // line is addx n
+            val number = line.split(' ')[1].toInt()
+            println("addx*$number*")
+            register.addX(number)
+        }
+        else { // line is noop
+            println("noop")
+            register.noOp()
+        }
+    }
+
+    var sumOfStrengths = 0
+    for (i in arrayOf(20, 60, 100, 140, 180, 220)){
+        sumOfStrengths += register.mapOfStrengths[i]!!
+    }
+
     var text = "--- Part One ---\n"
-    text += "${input[0]}\n"
+    text += "The sum of signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles is: $sumOfStrengths"
     println(text)
 
 } // ---------------------------------------- fun day10Part1(input: String) {
 
+
 fun day10Part2(input: String) {
-    //
-    var text = "--- Part Two ---\n"
+    // Part 2 What eight capital letters appear on your CRT?
+    var text = "--- Part Two ---"
     text += "${input[0]}\n"
+
     println(text)
 
 } // ---------------------------------------- fun day10Part2(input: String) {
+
+
+class Clock (time: Int = 0){
+    var time : Int = time
+        private set
+
+    fun tick(cycles: Int = 1){
+        check(cycles >= 1){"Error: Class Clock -> are you traveling faster than light?"}
+        time += cycles
+    }
+
+    override fun toString(): String {
+        val hours: Int = time / 3600
+        val minutes : Int = (time - hours * 3600) / 60
+        val seconds : Int = time - hours * 3600 - minutes * 60
+        return "${String.format("%02d", hours)}:${String.format("%02d", minutes)}:${String.format("%02d", seconds)}"
+    } // ---------------------------------------- override fun toString(): String {
+
+} // ---------------------------------------- class clock (time: Int = 0){
+
+
+class Register(private var value: Int = 1){
+    private var clock = Clock()
+    val mapOfStrengths = mutableMapOf(0 to 0)
+
+    fun noOp (){
+        tick()
+        println("---------------------------------------- Dentro de noOp. value:$value --- time:${clock.time}")
+    } // ---------------------------------------- fun noOp (){
+
+    fun addX(n: Int = 0){
+        tick(2)
+        this.value += n
+        println("---------------------------------------- Dentro de addX. value:$value --- time:${clock.time}")
+    } // ---------------------------------------- fun addX(n: Int = 0){
+
+    private fun tick(cycles: Int = 1){
+        check(cycles >= 1){"Error: Class Clock -> are you traveling faster than light?"}
+        for (t in 1..cycles){
+            clock.tick()
+            mapOfStrengths[clock.time] = this.value * this.clock.time
+            println(
+                "  --- Cycle:"  + String.format("% 2d", clock.time) +
+                        "  ---  Value:" + String.format("% 2d", value) +
+                        "  ---  Strength signal:" + String.format("% 2d", mapOfStrengths[clock.time]) +
+                        " --- "
+            )
+        }
+    }
+
+} // ---------------------------------------- class Register(x: Int = 0){
